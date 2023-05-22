b569b2a75f18ca0739bbd142856ae4f4607716b7
Ismailduman <issmailduman@gmail.com>
2023-05-22 16:36:29 +0200

    q_14

---
+++ src/Q_14.java
@@ -0,0 +1,7 @@
+public class Q_14 {
+    public static void main(String[] args) {
+        String str= "Sweet Sweat";
+        String str2= str.trim().charAt(6)+" "+str.indexOf(" Swe",2);
+        System.out.println(str2);
+    }
+}
